ed9d4ba2cbaa2ccf8fc2440990cca1fd96437813
kevin578 <kbriggs@2600-6c64-607f-f8a6-0c42-a10c-4f35-1bab.dhcp6.chtrptr.net>
2017-10-09 10:59:08 -0400

    set up the table view

--- schedule master/ViewController.swift
+++ schedule master/ViewController.swift
@@ -8,8 +8,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    let list = ["Milk", "Honey", "Bread", "Tacos", "Tomatoes"]
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return(list.count)
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "cell")
+        cell.textLabel?.text = list[indexPath.row]
+        
+        return(cell)
+    }
+    
 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
