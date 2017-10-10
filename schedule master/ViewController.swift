63b49108305ef4d035656a57054c5c51aceca533
kevin578 <kbriggs@aapl000894.local>
2017-10-10 10:08:22 -0400

    fixed contstraints

--- schedule master/ViewController.swift
+++ schedule master/ViewController.swift
@@ -9,42 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    var list: [String] = []
-    
+
+    let list = ["apples", "bread", "chicken"]
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return(list.count)
+          return(list.count)
     }
-    
-    @IBOutlet weak var textAdd: UITextField!
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "cell")
+        let cell = UITableViewCell( style: UITableViewCellStyle.default, reuseIdentifier: "cell")
         cell.textLabel?.text = list[indexPath.row]
-        
         return(cell)
-    }
-    
-
-    @IBOutlet weak var tableView: UITableView!
-    
+        }
     
     override func viewDidLoad() {
+        
         super.viewDidLoad()
     }
 
     @IBAction func AddReminder(_ sender: Any) {
-        list.append(textAdd.text!)
-        tableView.reloadData()
-        textAdd.text = ""
-        textAdd.resignFirstResponder()
     }
     
     
     @IBAction func Edit(_ sender: Any) {
-
-    }
     
+    }
 }
-
-
 
