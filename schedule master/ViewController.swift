79babcee778eb8e652d613a204e548f02ada398d
Bancroft <jseiple@aapl000609.local>
2017-10-10 09:16:19 -0400

    added table view

--- schedule master/ViewController.swift
+++ schedule master/ViewController.swift
@@ -8,9 +8,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    let list = ["apples", "bread", "chicken"]
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+          return(list.count)
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell( style: UITableViewCellStyle.default, reuseIdentifier: "cell")
+        cell.textLabel?.text = list[indexPath.row]
+        return(cell)
+        }
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         label.text = "hello!!"
     }
