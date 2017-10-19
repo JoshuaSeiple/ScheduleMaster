bcdcaf68d965c1dccdf3b75930aa881a16af4c90
Bancroft <jseiple@aapl000609.local>
2017-10-19 09:13:44 -0400

    added working text field

--- schedule master/ViewController.swift
+++ schedule master/ViewController.swift
@@ -10,23 +10,28 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
-    let list = ["apples", "bread", "chicken"]
+    var list:[String] = []
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
           return(list.count)
     }
     
+    @IBOutlet weak var textInput: UITextField!
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell( style: UITableViewCellStyle.default, reuseIdentifier: "cell")
         cell.textLabel?.text = list[indexPath.row]
         return(cell)
         }
     
+    @IBOutlet weak var tasks: UITableView!
     override func viewDidLoad() {
         
         super.viewDidLoad()
     }
 
     @IBAction func AddReminder(_ sender: Any) {
+        list.append(textInput.text!)
+        tasks.reloadData()
+        textInput.text! = ""
     }
     
     
